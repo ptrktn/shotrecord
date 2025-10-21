ed1dadaf30f70a5ac27aa869e6f0152117dfeb3a
Petteri Kettunen <ptrktn@users.noreply.github.com>
2025-10-21 22:15:11 +0900

    Bootstrap SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template, request
-# from app.pairing import generate_pairing
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Series, Shots
 
+# Create the Flask application instance
 app = Flask(__name__)
+
+# Configure the database
+# The database URI is loaded from an environment variable for security
+# and defaults to a local SQLite file for development.
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URLX', 
+    'sqlite:///' + os.path.join(basedir, 'instance', 'app.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the Flask-SQLAlchemy extension
+db.init_app(app)
+with app.app_context():
+    # Create the database tables from the models defined in models.py
+    db.create_all()
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#     db.init_app(app)
+
+#     with app.app_context():
+#         # from models import Series, Shots
+#         db.create_all()
+
+#     return app
 
 @app.route("/", methods=["GET"])
 def index():
---
+++ models.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from datetime import datetime
+
+# Create the SQLAlchemy object
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+class Series(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    total_points = db.Column(db.Float, nullable=False)
+    total_t = db.Column(db.Float, nullable=True)
+
+    def __repr__(self):
+        return f"<Series {self.id}>"
+
+class Shots(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'), nullable=False)
+    hit = db.Column(db.Integer, nullable=False)
+    points = db.Column(db.Float, nullable=False)
+    shotnum = db.Column(db.Integer, nullable=False)
+    x = db.Column(db.Integer, nullable=True)
+    y = db.Column(db.Integer, nullable=True)
+    t = db.Column(db.Float, nullable=True)
+    series = db.relationship('Series', backref=db.backref('shots', lazy=True))
+
+    def __repr__(self):
+        return f"<Shot {self.id} in Series {self.series_id} at ({self.x}, {self.y})>"
